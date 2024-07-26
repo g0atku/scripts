1f38b7bdebea6e8ca7b4526753b3d7029381ed51
g0atku <153930694+g0atku@users.noreply.github.com>
2024-07-26 11:27:29 +0500

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,6 +1,7 @@
 local Whitelist = {
     d405342167515529226 = {
-        Solara = 'e0716c45cf97fe7acb4db087f5c3d9a7fee60789c533a9574e35f961b82dbad8'
+        Solara = 'e0716c45cf97fe7acb4db087f5c3d9a7fee60789c533a9574e35f961b82dbad8',
+        Wave = '4cf31cbd1a5970e56d3fc84ed41d518f5763627e4acf6286ac06dc1f7cdc0fdfd58274e99cb60fac9a8b5fb56eb2a2d3'
     }
 }
 return Whitelist
