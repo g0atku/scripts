59403fd5124a3ecbd5b473f55610e1efbd4ae60f
g0atku <153930694+g0atku@users.noreply.github.com>
2024-07-30 13:20:44 +0500

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,7 +1,49 @@
 local Whitelist = {
-    d405342167515529226 = {
+    p405342167515529226 = {
         Solara = 'e0716c45cf97fe7acb4db087f5c3d9a7fee60789c533a9574e35f961b82dbad8',
-        Wave = '4cf31cbd1a5970e56d3fc84ed41d518f5763627e4acf6286ac06dc1f7cdc0fdfd58274e99cb60fac9a8b5fb56eb2a2d3'
+        Wave = '4cf31cbd1a5970e56d3fc84ed41d518f5763627e4acf6286ac06dc1f7cdc0fdfd58274e99cb60fac9a8b5fb56eb2a2d3' -- Finger
+    },
+    d785192705465647135 = {
+        Solara = '66ed71030c4ed5c62c743176c2fbd8b1ba6d6b879599a0b326677117a89e2799' -- Palex
+    },
+    d1013784222865051658 = {
+        Solara = '54bbd97b33c29f05ba626658e930cf0131fb0f58fbd3b871fab62f0cee9e3367' -- Xyme
+    },
+    d452463352891834368 = {
+        Solara = '3e7e5c5f644276fa5867386198aded95fd09831b9fc315e3c29477d6789fb20d' -- Gytis
+    },
+    d368130451199492114 = {
+        Solara = 'fd3ce2791d9bc48c16dda67f51864b2a1adec2323248221b104a63f09266c0a1' -- freedom
+    },
+    d566734199503978526 = {
+        Solara = 'f163d5f75184fbc8b90042510dcf03312ba1f32aa1e6b14a6579ac865c4c195c' -- Blue
+    },
+    d876587804962996275 = {
+        Solara = 'cffb558ef3e286274bfec21f905618377693ce6c220cf05b2bace6114028ffd3' -- kise
+    },
+    d762347301757779969 = {
+        Solara = 'c22bc8af0566568320991c8e858d55a4ba8ada7ff5da64bd7ff561367e82482e' -- Gh
+    },
+    d848526094437187624 = {
+        Solara = 'f171329de54fdb5fab2babf103fc63802328fc7590a22427d8e0f1f62aac9170' -- kap
+    },
+    d1241290266380603392 = {
+        Solara = '262d84c1381c994b5ee87938a20ee1c26c4bb81ab6b96462b56592657eca7f54' -- Mango
+    },
+    d847745697285799938 = {
+        Solara = '9f85045dae8ddce7eb1226d010984af9c480067d310a188f194292d2c325aa09' -- sbcz
+    },
+    d1033459507885514784 = {
+        Solara = '42bf30b7912e68f70a522c72f520871b2ba0c3c8975373973eb4d0cc85447333' -- anti null
+    },
+    d332693258629021697 = {
+        Solara = 'ccbcb43d46e6c5de80f2b52ab2226ac0bc0f788c64f25bbdcc98f3cc80566457' -- emperorbloo
+    },
+    d1124689855746670602 = {
+        Solara = '328f17eea2af9496821eae5a559aadf57c0c892af1378413170a4e5233ab4ecb' -- dar dar
+    },
+    d379009328989995008 = {
+        Solara = 'ee6214957a43c86ad563b73b94a2a402ccf268a1c4ea76bb3d29538e94920050' -- AnimeIIInik
     }
 }
 return Whitelist
