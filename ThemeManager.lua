1dd24ad8e9f5ef22426c9b3302fe57795f02381a
g0atku <153930694+g0atku@users.noreply.github.com>
2024-08-01 15:54:34 +0500

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'VevoHub'
+	ThemeManager.Folder = 'VevoHub '..version
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
