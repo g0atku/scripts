69ee16da4e5964aa0fac2fe1b2b1df222bb80814
g0atku <153930694+g0atku@users.noreply.github.com>
2024-08-09 20:39:55 +0500

    Update HMM.lua

--- Locked/HMM.lua
+++ Locked/HMM.lua
@@ -4,9 +4,9 @@ local oldNameCall
 oldNameCall = hookmetamethod(game, '__namecall', function(self, ...)
     local args = {...}
     local method = getnamecallmethod()
-    if self == MyPlayer and method:lower() == 'kick' then
+    if self == MyPlayer and method:lower() == 'kick' and not checkcaller() then
         return
-    elseif self.Name == 'Emotecs' and method == 'FireServer' then
+    elseif self.Name == 'Emotecs' and method == 'FireServer' and not checkcaller() then
         return
     elseif IsAutoDribble and self.Name == 'ChestBump' and method == 'FireServer' then
         args[2] = args[2] / 1.5
@@ -14,10 +14,10 @@ oldNameCall = hookmetamethod(game, '__namecall', function(self, ...)
         if args[21] then
             args[21] = args[21] * CurveMulti
         end
-    elseif M2HBE then
+    elseif M2HBE and not checkcaller() then
         args[12] = true
         args[13] = true
-    elseif IsAutoM2 and self.Name == 'Ragdoll' and method == 'FireServer' then
+    elseif IsAutoM2 and self.Name == 'Ragdoll' and method == 'FireServer' and not checkcaller() then
         return
     end
     return oldNameCall(self, unpack(args))
